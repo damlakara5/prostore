47376dfe9e30d4793e5d13c3bba8ee3afa83362a
damlakara5 <kdamla41@gmail.com>
2025-02-06 16:51:34 +0300

    first commit

---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,23 @@
+'use server'
+import { convertToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { prisma } from "@/db/prisma";
+
+//Get latest products
+
+export async function getLatestProducts() {
+    const data = await prisma.product.findMany({
+        take: LATEST_PRODUCTS_LIMIT,
+        orderBy: {createdAt: 'desc'}
+    });
+
+    return convertToPlainObject(data);
+}
+
+
+//get single product by its slug
+export async function getProductBySlug(slug: string) {
+    return await prisma.product.findFirst({
+        where: {slug: slug}
+    })
+}   
